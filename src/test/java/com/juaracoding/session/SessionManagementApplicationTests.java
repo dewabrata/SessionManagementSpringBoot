4dcf4013132a129bcbc042291798cdaf30c7a1f5
dewabrata <dewabrata@gmail.com>
2021-07-14 10:25:59 +0700

    first commit

---
+++ src/test/java/com/juaracoding/session/SessionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
